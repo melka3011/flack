9b994f04843430e6da65ee21f8f458dd891f0a27
Luka <luka.melcic@gmail.com>
2020-07-02 09:59:14 +0400

    local storage for login - some sockets implemented

--- application.py
+++ application.py
@@ -16,10 +16,23 @@ users = ['Luka','Marko']
 def index():
     return render_template('index.html')
 
+# flask socketio's message handler
 @socketio.on('message')
 def handle_message(message):
     print('received message: ' + message)
     send(message, broadcast=True)
+
+@socketio.on('new_user')
+def new_user(data):
+    #checking the the users list if the display name is taken
+    displayname = data['displayname']
+    if displayname in users:
+        error = 'Display name already in use. Please try another one,'
+    else:
+        error = ''
+        users.append(displayname)
+    emit('add_user',{'displayname':displayname,'error':error})
+
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/js/main.js
+++ static/js/main.js
@@ -3,13 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
+    //Collect users local storage
+    var user_storage = window.localStorage;
+
     // When connected, configure buttons
     socket.on('connect', () => {
         socket.send('user has connected');
+        //check if user is in local storage
+        if (!user_storage.getItem('displayname')){
+
+            document.getElementById("login_wrapper").style.display = 'flex';
+            document.getElementById("chatwindow").style.display = 'none';
+
+        };
+        if (!user_storage.getItem('channel')){
+            user_storage.setItem('channel','/general')
+        }
+        
+        // TODO set general chatroom if no chatroom selected
     });
 
     socket.on('message', function(message) {
-            
+
             const li = document.createElement('li');
             li.innerHTML = message;
 
@@ -18,13 +33,52 @@ document.addEventListener('DOMContentLoaded', () => {
             return false;
     });
 
+    socket.on('add_user', function(data){
+
+        if (data["error"] != ""){
+              //show error that username is taken
+            alert('Username is taken, try again');
+
+            return false;
+        }
+        else{
+            //add user to local storage and enable chatrooms
+            user_storage.setItem('displayname', data['displayname']);
+            user_storage.setItem('channel', '/general');
+
+            document.getElementById("login_wrapper").style.display = 'none';
+            document.getElementById("chatwindow").style.display = 'flex';
+        }
+    });
+    socket.on('disconnect', () => {
+        socket.send('user disconnected');
+    });
+
     document.querySelector('#sendbutton').onclick = () => {
 
             const msg = document.querySelector('#myMessage').value;
 
             socket.send(msg);
-
+            
             document.querySelector('#myMessage').value = '';
+    };
+
+    document.querySelector('#logout').onclick = () => {
+        // clears the whole user local storage    
+        localStorage.clear();
+        socket.emit('disconnect',{'msg':'user disconnected'});
+        location.reload();
+
+    };
+
+    document.querySelector('#myForm').onsubmit = function() {
+
+            // Collect display name from the form
+            const displayname = document.getElementById('login').value;
+            console.log('form has been submitted:'+displayname);
+            socket.emit('new_user',{'displayname':displayname});
+
+            return false;
     };
 });
 
